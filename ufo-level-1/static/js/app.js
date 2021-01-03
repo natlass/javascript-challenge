6c2d9529ebbd08761d026d318c415296c7d7d535
nlass <natlassen@gmail.com>
2021-01-03 15:44:24 -0500

    fixed file issue

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -2,6 +2,7 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
 // Getting a reference to the button on the page
 var button = d3.select("#filter-btn");
 
@@ -21,7 +22,7 @@ function runEnter() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
   
-  // Select the input element and get the raw HTML node
+  // Select the input element and get the raw HTML
   var inputElement = d3.select("#datetime");
 
   // Get the value property of the input element
@@ -45,33 +46,3 @@ function runEnter() {
 });
 
 }
-
-  
-  /*
-  // BONUS: Calculate summary statistics for the age field of the filtered data
-
-  // First, create an array with just the age values
-  var ages = filteredData.map(person => person.age);
-
-  // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-  var mean = math.mean(ages);
-  var median = math.median(ages);
-  var mode = math.mode(ages);
-  var variance = math.var(ages);
-  var standardDeviation = math.std(ages);
-
-  // Then, select the unordered list element by class name
-  var list = d3.select(".summary");
-
-  // remove any children from the list to
-  list.html("");
-
-  // append stats to the list
-  list.append("li").text(`Mean: ${mean}`);
-  list.append("li").text(`Median: ${median}`);
-  list.append("li").text(`Mode: ${mode}`);
-  list.append("li").text(`Variance: ${variance}`);
-  list.append("li").text(`Standard Deviation: ${standardDeviation}`);
-};
-
-*/
